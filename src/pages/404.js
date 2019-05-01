fd44a2d302367035097899847624550b251685b6
Thijs Krooswijk <t.krooswijk@gmail.com>
2019-05-01 17:24:06 +0200

    Edit 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,26 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Heading from "../components/heading"
+import Text from "../components/text"
+import Box from "../components/box"
+import Link from "../components/link"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Box
+      m={"auto"}
+      p={4}
+      width={[1]}
+      style={{
+        maxWidth: "1600px",
+      }}
+    >
+      <Heading is={"h1"}>NOT FOUND</Heading>
+      <Text is={"p"}>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+      <Link to={"/"}>Please Try this Link</Link>
+    </Box>
   </Layout>
 )
 
