e1b2aa4998682991736543b1372b1625dbb0d1a8
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-08-05 12:42:15 +0200

    chore: add DeepPartial to Data

--- src/themes/flow/data/blog-data.tsx
+++ src/themes/flow/data/blog-data.tsx
@@ -1,6 +1,24 @@
+import { DeepPartial } from '@wheelroom/core'
 import { BlogSectionData } from '@wheelroom/wheel-blog'
 
-export const blogSectionData: BlogSectionData = {
-  blog: {},
-  list: {},
+export const blogSectionData: DeepPartial<BlogSectionData> = {
+  blog: {
+    hideAbstract: false,
+    hideAuthors: false,
+    hideCategories: false,
+    hideDate: false,
+    hideHeader: false,
+    hideHeading: false,
+    hideMedia: true,
+    hideRichText: false,
+  },
+  list: {
+    hideAbstract: false,
+    hideCategories: false,
+    hideContent: false,
+    hideDate: false,
+    hideHeading: false,
+    hideMedia: true,
+    hideText: false,
+  },
 }
