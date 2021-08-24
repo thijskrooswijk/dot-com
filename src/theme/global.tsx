eea5ae39aaf8f9eb243235e40390d5d294462638
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-08-24 22:42:23 +0200

    chore: cleanup global styling

--- src/theme/global.tsx
+++ src/theme/global.tsx
@@ -3,196 +3,24 @@ import { css } from '@emotion/react'
 export const globalStyle = css(`
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    color: white;
+    color: #fff;
     background: #222;
-  }
-
-  .gatsby-image-wrapper {
-    display: flex;
-    flex: 1 0 25%;
-    width: 100%;
+    background: radial-gradient(68.13% 100% at 100% 0,#dd5ce5 0,rgba(221,92,229,0) 100%),linear-gradient(95.52deg,#000,rgba(0,0,0,0)),linear-gradient(0deg,#5436da,#5436da),#6e6e80;
   }
   
   .right {
-    animation: spin-right linear 8s infinite;
-  }
-  
-  .right-negative {
-    animation: spin-left linear 8s infinite;
+    animation: rotate-right linear 8s infinite;
   }
   
   .left {
-    animation: spin-left linear 8s infinite;
+    animation: rotate-left linear 8s infinite;
   }
   
-  .left-negative {
-    animation: spin-right linear 8s infinite;
-  }
-  
-  @keyframes spin-right {
+  @keyframes rotate-right {
     100% { transform: rotate(360deg); }
   }
   
-  @keyframes spin-left {
+  @keyframes rotate-left {
     100% { transform: rotate(-360deg); }
-  }
-  
-  @-webkit-keyframes ani {
-    0 % {opacity: 0}
-    5% {opacity: 1}
-    90% {transform: scale(.9); opacity: 1}
-    100% {transform: scale(1); opacity: 0}
-  }
-
-  @-webkit-keyframes blob-left-top-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(-100px, 0);
-              transform: scale(0.9) translate(-100px, 0);
-    }
-    62% {
-      -webkit-transform: scale(0.7) translate(-100px, -100px);
-              transform: scale(0.7) translate(-100px, -100px);
-    }
-    94% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @keyframes blob-left-top-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(-100px, 0);
-              transform: scale(0.9) translate(-100px, 0);
-    }
-    62% {
-      -webkit-transform: scale(0.7) translate(-100px, -100px);
-              transform: scale(0.7) translate(-100px, -100px);
-    }
-    94% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @-webkit-keyframes blob-right-top-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(100px, 0);
-              transform: scale(0.9) translate(100px, 0);
-    }
-    64% {
-      -webkit-transform: scale(0.7) translate(100px, -100px);
-              transform: scale(0.7) translate(100px, -100px);
-    }
-    96% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @keyframes blob-right-top-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(100px, 0);
-              transform: scale(0.9) translate(100px, 0);
-    }
-    64% {
-      -webkit-transform: scale(0.7) translate(100px, -100px);
-              transform: scale(0.7) translate(100px, -100px);
-    }
-    96% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @-webkit-keyframes blob-left-bottom-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(-100px, 0);
-              transform: scale(0.9) translate(-100px, 0);
-    }
-    66% {
-      -webkit-transform: scale(0.7) translate(-100px, 100px);
-              transform: scale(0.7) translate(-100px, 100px);
-    }
-    98% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @keyframes blob-left-bottom-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(-100px, 0);
-              transform: scale(0.9) translate(-100px, 0);
-    }
-    66% {
-      -webkit-transform: scale(0.7) translate(-100px, 100px);
-              transform: scale(0.7) translate(-100px, 100px);
-    }
-    98% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @-webkit-keyframes blob-right-bottom-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(100px, 0);
-              transform: scale(0.9) translate(100px, 0);
-    }
-    68% {
-      -webkit-transform: scale(0.7) translate(100px, 100px);
-              transform: scale(0.7) translate(100px, 100px);
-    }
-    100% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-  }
-  
-  @keyframes blob-right-bottom-anim {
-    0% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
-    33% {
-      -webkit-transform: scale(0.9) translate(100px, 0);
-              transform: scale(0.9) translate(100px, 0);
-    }
-    68% {
-      -webkit-transform: scale(0.7) translate(100px, 100px);
-              transform: scale(0.7) translate(100px, 100px);
-    }
-    100% {
-      -webkit-transform: scale(1.1) translate(0, 0);
-              transform: scale(1.1) translate(0, 0);
-    }
   }
 `)
