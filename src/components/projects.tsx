67bd7421c0583f6c4c5187c9310f3ec4cd72b1e8
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-08-25 21:33:16 +0200

    chore: fix html attr issue

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,21 +1,45 @@
-import { A, Li, Span, Ul } from '@wheelroom/any/react'
+import { A, Div, Li, Span, Ul } from '@wheelroom/any/react'
+import { Fragment } from 'react'
 import { contentModel } from '../data/content'
-import { linkStyle, projectListItemStyle, projectListStyle } from '../theme/style'
+import {
+  linkStyle,
+  projectListItemStyle,
+  projectListStyle,
+} from '../theme/style'
 
 interface ProjectsProps {
   model: contentModel['projects']
 }
 
+const Abstract = ({ model }) => {
+  return (
+    <>
+      <Span>{model.client}</Span>
+      {` `}
+      {model.description}
+    </>
+  )
+}
+
+const ConditionalRendering = ({ model, ...props }) =>
+  model.url ? (
+    <A href={model.url} css={linkStyle}>
+      {props.children}
+    </A>
+  ) : (
+    <Fragment {...props} />
+  )
+
 export const Projects = ({ model }: ProjectsProps) => {
   const projects = model
-  const projectList = projects.map((project, index) => (
-    <Li key={index} css={projectListItemStyle}>
-      <A href={project.url} css={linkStyle}>
-        <Span>{project.client}</Span>
-        {` `}
-        {project.description}
-      </A>
-    </Li>
-  ))
+  const projectList = projects.map((project, index) => {
+    return (
+      <Li key={index} css={projectListItemStyle}>
+        <ConditionalRendering model={project}>
+          <Abstract model={project} />
+        </ConditionalRendering>
+      </Li>
+    )
+  })
   return <Ul css={projectListStyle}>{projectList}</Ul>
 }
