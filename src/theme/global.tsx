84cb61dd6176eddd3311ccb52408cb23445f1297
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-08-28 22:01:42 +0200

    chore: generate random background gradients

--- src/theme/global.tsx
+++ src/theme/global.tsx
@@ -1,9 +1,29 @@
 import { css } from '@emotion/react'
 
+const generateColorCode = () => Math.round(Math.random() * 255)
+
+const createRgba = () => {
+  return (
+    'rgba(' +
+    generateColorCode() +
+    ',' +
+    generateColorCode() +
+    ',' +
+    generateColorCode() +
+    ', 1)'
+  )
+}
+
 export const globalStyle = css(`
   body {
-    background: #222;
-    background: radial-gradient(68.13% 100% at 100% 0,#dd5ce5 0,rgba(221,92,229,0) 100%),linear-gradient(95.52deg,#000,rgba(0,0,0,0)),linear-gradient(0deg,#5436da,#5436da),#6e6e80;
+    background: #000;
+    background: radial-gradient(68.13% 100% at 100% 0,` +
+    createRgba() +
+    ` 0,rgba(0,0,0,0) 100%), linear-gradient(95.52deg,#000,rgba(0,0,0,0)), linear-gradient(95.52deg,` +
+    createRgba() +
+    `,` +
+    createRgba() +
+    `);
     color: #fff;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     font-size: 18px;
