262763f176cafdb993e83ed9f4bbe8e7f679c27e
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-05-06 16:36:47 -0700

    chore: add React Suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,31 @@
-import { graphql, PageProps, useStaticQuery } from 'gatsby'
-import { Clock } from '../components/clock'
-import { Content } from '../components/content'
-import { Section } from '../components/elements'
-import { Layout } from '../components/layout'
-import { Seo } from '../components/seo'
-import { Template } from '../components/template'
-import { socials } from '../data/content'
-import { ContentModel } from '../data/models'
-import { StyleObject } from '../lib/css-types'
-import { mq } from '../theme/mq'
+import * as React from "react"
+
+import { graphql, PageProps, useStaticQuery } from "gatsby"
+import { Content } from "../components/content"
+import { Section } from "../components/elements"
+import { Layout } from "../components/layout"
+import { Seo } from "../components/seo"
+import { Template } from "../components/template"
+import { platforms } from "../data/content"
+import { ContentModel } from "../data/models"
+import { StyleObject } from "../lib/css-types"
+import { mq } from "../theme/mq"
+
+const Clock = React.lazy(() => import("../components/clock"))
 
 const sectionStyle = mq({
-  alignItems: 'center',
-  display: 'grid',
+  alignItems: "center",
+  display: "grid",
   gap: 16,
-  gridTemplateColumns: ['repeat(1, 1fr)', 'repeat(2, 1fr)'],
-  justifyItems: 'center',
-  minHeight: '100vh',
+  gridTemplateColumns: ["repeat(1, 1fr)", "repeat(2, 1fr)"],
+  justifyItems: "center",
+  minHeight: "100vh",
 } as StyleObject)
 
 type IndexQueryProps = ContentModel
 type IndexPageProps = PageProps<IndexQueryProps>
 
 const IndexPage = (props: IndexPageProps) => {
-  let { data } = props
   const contentfulData = useStaticQuery(graphql`
     query {
       contentfulIndex {
@@ -36,7 +38,8 @@ const IndexPage = (props: IndexPageProps) => {
       }
     }
   `)
-  data = { ...contentfulData.contentfulIndex, socials }
+  const content = { ...contentfulData.contentfulIndex, platforms }
+
   return (
     <Layout>
       <Seo
@@ -45,8 +48,10 @@ const IndexPage = (props: IndexPageProps) => {
       />
       <Template>
         <Section css={sectionStyle}>
-          <Content model={data} />
-          <Clock />
+          <Content model={content} />
+          <React.Suspense fallback={<></>}>
+            <Clock />
+          </React.Suspense>
         </Section>
       </Template>
     </Layout>
