51cf7d804ccb11e1393773e788b64749fca5f5a3
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-05-06 16:34:58 -0700

    chore: refactor Content component

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,18 +1,18 @@
-import { renderRichText } from 'gatsby-source-contentful/rich-text'
-import { ContentModel } from '../data/models'
-import { StyleObject } from '../lib/css-types'
-import { Box } from './box'
-import { Clients } from './clients'
-import { Header } from './header'
-import { options } from './rich-text'
-import { Socials } from './socials'
-import { Text } from './text'
+import { renderRichText } from "gatsby-source-contentful/rich-text"
+import { ContentModel } from "../data/models"
+import { StyleObject } from "../lib/css-types"
+import { Box } from "./box"
+import { Clients } from "./clients"
+import { Header } from "./header"
+import { options } from "./rich-text"
+import { Platforms } from "./platforms"
+import { Text } from "./text"
 
 const contentStyle: StyleObject = {
-  boxSizing: 'border-box',
+  boxSizing: "border-box",
   maxWidth: 720,
   padding: 24,
-  width: '100%',
+  width: "100%",
 }
 
 const footnoteStyle: StyleObject = {
@@ -24,14 +24,16 @@ interface ContentProps {
 }
 
 export const Content = ({ model }: ContentProps) => {
+  const { heading, abstract, clients, footnote, platforms } = model
+
   return (
     <Box css={contentStyle}>
-      <Header model={model.heading} />
-      <main>
-        {model.abstract && renderRichText(model.abstract, options)}
-        <Clients model={model.clients} />
-        <Text css={footnoteStyle}>{model.footnote}</Text>
-        <Socials model={model.socials} />
+      <Header>{heading}</Header>
+      <main role="main" id="main">
+        {abstract && renderRichText(abstract, options)}
+        <Clients model={clients} />
+        <Text css={footnoteStyle}>{footnote}</Text>
+        <Platforms model={platforms} />
       </main>
     </Box>
   )
