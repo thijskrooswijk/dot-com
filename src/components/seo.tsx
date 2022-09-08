125d4033a40c20a9e0eb10bc6b168732ae0284ae
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-09-07 22:54:44 -0700

    chore: remove uuid

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -8,7 +8,6 @@ export const Seo = ({
   title = "",
   slug = "",
 }) => {
-  const uuid = React.useId()
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -39,33 +38,32 @@ export const Seo = ({
 
   return (
     <React.Fragment>
-      <title key={uuid}>{seo.title}</title>
-      <link key={uuid} rel="canonical" href={seo.url} />
+      <title>{seo.title}</title>
+      <link rel="canonical" href={seo.url} />
 
       {/* Primary Meta Tags */}
-      <meta key={uuid} name="title" content={seo.title} />
-      <meta key={uuid} name="description" content={seo.description} />
-      {/* <meta key={uuid} name="image" content={seo.image} /> */}
+      <meta name="title" content={seo.title} />
+      <meta name="description" content={seo.description} />
+      {/* <meta name="image" content={seo.image} /> */}
       <meta
-        key={uuid}
         name="keywords"
         content={seo.keywords ? seo.keywords.join(", ") : undefined}
       />
 
       {/* Open Graph / Facebook  */}
-      <meta key={uuid} property="og:type" content="website" />
-      <meta key={uuid} property="og:url" content={seo.url} />
-      <meta key={uuid} property="og:title" content={seo.title} />
-      <meta key={uuid} property="og:description" content={seo.description} />
-      {/* <meta key={uuid} property="og:image" content={seo.image} /> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      {/* <meta property="og:image" content={seo.image} /> */}
 
       {/* Twitter */}
-      <meta key={uuid} name="twitter:card" content="summary_large_image" />
-      <meta key={uuid} name="twitter:creator" content={seo.author} />
-      <meta key={uuid} name="twitter:url" content={seo.url} />
-      <meta key={uuid} name="twitter:title" content={seo.title} />
-      <meta key={uuid} name="twitter:description" content={seo.description} />
-      {/* <meta key={uuid} name="twitter:image" content={seo.image} /> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={seo.author} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      {/* <meta name="twitter:image" content={seo.image} /> */}
     </React.Fragment>
   )
 }
